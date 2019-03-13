9c4bd037bd9cad9eaeb3b321dbff25e636c16ff1
Rushikesh Patel <rushikeshpatelc502@gmail.com>
2019-03-13 18:59:49 +0530

    fixed debug make error
    fixed error: 'ip_addr_t' has no member named 'addr'

--- glue-lwip/lwip-git.c
+++ glue-lwip/lwip-git.c
@@ -143,9 +143,9 @@ static void new_display_netif (struct netif* netif)
 	if (netif->hwaddr_len == 6)
 		display_mac(netif->hwaddr);
 	new_display_netif_flags(netif->flags);
-	display_ip32(" ip=", netif->ip_addr.addr);
-	display_ip32(" mask=", netif->netmask.addr);
-	display_ip32(" gw=", netif->gw.addr);
+	display_ip32(" ip=", netif->ip_addr.u_addr.ip4.addr);
+	display_ip32(" mask=", netif->netmask.u_addr.ip4.addr);
+	display_ip32(" gw=", netif->gw.u_addr.ip4.addr);
 	uprint("\n");
 }
 
