2f481dc6b4d662bfc2905d2db3554aadad3989e4
Rushikesh Patel <rushikeshpatelc502@gmail.com>
2019-03-14 14:19:22 +0530

    fixed debug make error (#30)
    * fixed debug make error
    
    fixed error: 'ip_addr_t' has no member named 'addr'
    
    * ipv4&v6 compatible print

--- glue-lwip/lwip-git.c
+++ glue-lwip/lwip-git.c
@@ -143,9 +143,9 @@ static void new_display_netif (struct netif* netif)
 	if (netif->hwaddr_len == 6)
 		display_mac(netif->hwaddr);
 	new_display_netif_flags(netif->flags);
-	display_ip32(" ip=", netif->ip_addr.addr);
-	display_ip32(" mask=", netif->netmask.addr);
-	display_ip32(" gw=", netif->gw.addr);
+	display_ip32(" ip=", ip_2_ip4(&netif->ip_addr)->addr);
+	display_ip32(" mask=", ip_2_ip4(&netif->ip_addr)->addr);
+	display_ip32(" gw=", ip_2_ip4(&netif->ip_addr)->addr);
 	uprint("\n");
 }
 
