af6738a22332a92256471e3d29d37a2700ec1298
David Gauchard <gauchard@laas.fr>
2019-07-25 15:50:13 +0200

    +napt: dhcp server: +api to set dns force copy of napt-forwarded packets to avoid crash

--- glue-lwip/open/lwipopts.h
+++ glue-lwip/open/lwipopts.h
@@ -767,6 +767,10 @@
 #define IP_FRAG                         0
 #endif /* !LWIP_IPV4 */
 
+#ifndef IP_NAPT
+#define IP_NAPT                         0
+#endif
+
 /**
  * IP_OPTIONS_ALLOWED: Defines the behavior for IP options.
  *      IP_OPTIONS_ALLOWED==0: All packets with IP options are dropped.
@@ -3490,6 +3494,13 @@
 /**
  * @}
  */
+
+/**
+ * NAPT_DEBUG: Enable debugging for NAPT.
+ */
+#ifndef NAPT_DEBUG
+#define NAPT_DEBUG                       LWIP_DBG_OFF
+#endif
 
 /**
  * LWIP_TESTMODE: Changes to make unit test possible
