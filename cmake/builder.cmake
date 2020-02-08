785de0a02b504b24683f8a43d4597e714c61d88a
Maxim Prokhorov <prokhorov.max@outlook.com>
2020-02-08 05:59:30 +0300

    Build with cmake

---
+++ cmake/builder.cmake
@@ -0,0 +1,90 @@
+cmake_minimum_required(VERSION 3.9)
+
+set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} "${CMAKE_SOURCE_DIR}/cmake/")
+set(CMAKE_C_STANDARD 99)
+
+message(STATUS "toolchain: " ${CMAKE_TOOLCHAIN_FILE})
+message(STATUS "programpath: " ${CMAKE_PROGRAM_PATH})
+
+# --- to always regen sources when running cmake:
+#   message(STATUS "Checkout ${LWIP_TAG}")
+#   execute_process(
+#       COMMAND ${LWIP_CLEAN_COMMAND}
+#   )
+#   execute_process(
+#       COMMAND git -C ${LWIP_DIR} checkout ${LWIP_TAG}
+#   )
+
+
+set(GLUE_C_FLAGS TCP_MSS=${TCP_MSS} LWIP_IPV6=${LWIP_IPV6} LWIP_FEATURES=${LWIP_FEATURES})
+
+# TODO: glue-lwip/lwip-err-t.h defines this!
+#       -DLWIP_NO_STDINT_H=1
+#       Should probably be here instead
+set (LWIP_DEFINITIONS
+    -DARDUINO
+    -D__ets__
+    -DICACHE_FLASH
+    -DLWIP_OPEN_SRC
+    -DLWIP_BUILD
+    -DUSE_OPTIMIZE_PRINTF
+)
+set (LWIP_INCLUDE_DIRS
+    "${ARDUINO_DIR}/tools/sdk/include"
+    "${ARDUINO_DIR}/tools/sdk/libc/xtensa-lx106-elf/include"
+    "${ARDUINO_DIR}/cores/esp8266"
+    "${LWIP_DIR}/src/include"
+    "${GLUE_DIR}/glue"
+    "${GLUE_DIR}/glue-esp"
+    "${GLUE_DIR}/glue-lwip"
+    "${GLUE_DIR}/glue-lwip/arduino"
+)
+set(LWIP_COMPILER_FLAGS
+    -g
+)
+
+include(${LWIP_DIR}/src/Filelists.cmake)
+
+# --- all the source code from the glue-lwip/...
+# --- TODO: arduino target does not need millis() stub
+add_library(${GLUE_VARIANT_NAME}-glue STATIC "")
+
+# --- we need to explicitly set these
+# --- TODO: target_compile_features?
+target_sources(${GLUE_VARIANT_NAME}-glue
+    PRIVATE
+    ${GLUE_DIR}/glue-lwip/lwip-git.c
+    ${GLUE_DIR}/glue-lwip/esp-dhcpserver.c
+    ${GLUE_DIR}/glue-lwip/esp-ping.c
+    ${GLUE_DIR}/glue-lwip/espconn.c
+    ${GLUE_DIR}/glue-lwip/espconn_buf.c
+    ${GLUE_DIR}/glue-lwip/espconn_tcp.c
+    ${GLUE_DIR}/glue-lwip/espconn_udp.c
+    ${GLUE_DIR}/glue/doprint.c
+    ${GLUE_DIR}/glue/uprint.c
+)
+target_include_directories(${GLUE_VARIANT_NAME}-glue BEFORE
+    PRIVATE
+    ${GLUE_DIR}/glue-lwip/
+    ${GLUE_DIR}/glue/
+    ${LWIP_INCLUDE_DIRS}
+)
+
+if($(GLUE_TARGET) EQUAL OPENSDK)
+    target_sources(${GLUE_VARIANT_NAME}-glue
+        PRIVATE
+        ${GLUE_DIR}/glue-lwip/esp-time.c
+        ${GLUE_DIR}/glue-lwip/esp-millis.c
+    )
+endif()
+
+target_compile_definitions(${GLUE_VARIANT_NAME}-glue PRIVATE ${LWIP_DEFINITIONS})
+target_compile_definitions(${GLUE_VARIANT_NAME}-glue PRIVATE ${GLUE_C_FLAGS})
+
+target_compile_definitions(lwipcore PRIVATE ${GLUE_C_FLAGS})
+
+target_link_libraries(${GLUE_VARIANT_NAME}-glue PUBLIC lwipcore)
+
+add_library(${GLUE_VARIANT_NAME} STATIC $<TARGET_OBJECTS:${GLUE_VARIANT_NAME}-glue> $<TARGET_OBJECTS:lwipcore>)
+
+install(TARGETS ${GLUE_VARIANT_NAME} ARCHIVE DESTINATION ".")
